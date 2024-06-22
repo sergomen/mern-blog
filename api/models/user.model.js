f722dd08abd33c80ddf9c635883bc295dc71a0dd
Sergei Iu <serg_omen@mail.ru>
2024-06-22 23:00:45 +0300

    create a test api route

---
+++ api/models/user.model.js
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    fullName: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    passwordHash: {
+        type: String,
+        required: true,
+    },
+    avatarUrl: String,
+    }, {timestamps: true}
+);
+
+const User = mongoose.model('User', userSchema);
+export default User;
